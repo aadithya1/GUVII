9f13371398c5f98e8b5636a92db97bc9ac6ec532
aadithya1 <54060091+aadithya1@users.noreply.github.com>
2019-08-13 15:07:29 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,8 @@
+m=(input(" "))
+if (m=="a" or m=="b" or m=="c" or m=="d" or m=="e" or m=="f" or m=="g" or m=="h" or m=="i" or m=="j" or m=="k" or m=="l" or m=="m" or m=="n" or m=="o" or m=="p" or m=="q" or m=="r" or m=="s" or m=="t" or m=="u" or m=="v" or m=="w" or m=="x" or m=="y" or m=="z"):
+    if (m=="a" or m=="e" or m=="i" or m=="o" or m=="u"):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
